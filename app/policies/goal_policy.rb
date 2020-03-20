cb401e09bfe522534fc108d716197253af5fe945
Pedro Ventura <pedro.ventura19g@gmail.com>
2020-03-20 19:26:32 +0000

    edit, destroy...

--- app/policies/goal_policy.rb
+++ app/policies/goal_policy.rb
@@ -16,4 +16,18 @@ class GoalPolicy < ApplicationPolicy
   def show?
     true
   end
+
+  def update?
+    user_is_owner?
+  end
+
+  def destroy?
+    user_is_owner?
+  end
+
+  private
+
+  def user_is_owner?
+    record.user == user
+  end
 end
